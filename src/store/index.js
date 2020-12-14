e9275499423200b84d9236ab8c9dae202594bf83
joseluisgs <joseluisgonsan@gmail.com>
2020-12-14 19:21:17 +0100

    Cofigurando Vuex y sus módulos

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,34 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+// Vamos a crear un módulo para cada modelo
+import messages from './messages';
+import rooms from './rooms';
+import user from './user';
+import utils from './utils';
+
 Vue.use(Vuex);
 
-export default new Vuex.Store({
+// Creamos la store central
+const store = new Vuex.Store({
   state: {
   },
   mutations: {
   },
   actions: {
+    checkAuth() { },
   },
+  // Cargamos los módulos
   modules: {
+    messages,
+    rooms,
+    user,
+    utils,
   },
 });
+
+// Exportamos
+export default store;
+
+// Comprobar si está autenticado
+store.dispatch('checkAuth');
---
+++ src/store/messages.js
@@ -0,0 +1,19 @@
+// Modelos de mensajes
+const state = {
+  messages: [],
+};
+
+const getters = {};
+
+const mutations = {};
+
+const actions = {};
+
+export default {
+  // Importante namespace para evitar problemas de importanción
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations,
+};
