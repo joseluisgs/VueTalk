6fe541eb32525649d6eea5742265754ab12d5148
joseluisgs <joseluisgonsan@gmail.com>
2020-12-20 10:25:12 +0100

    Firebase Auth login and register

--- src/services/Firebase/Auth.js
+++ src/services/Firebase/Auth.js
@@ -8,7 +8,7 @@ export default {
 
   /**
    * Devuelve el usuario activo
-   * @returns {User} Objeto User Google Auth
+   * @returns {user} Objeto User Google Auth
    */
   getUsuarioActivo() {
     return Auth.currentUser;
@@ -16,7 +16,7 @@ export default {
 
   /**
    * Realiza el login con Google: https://firebase.google.com/docs/auth/web/google-signin?hl=es_419
-   * @returns {User} Objeto User Google Auth
+   * @returns {user} Objeto User Google Auth
    */
   async loginGoogle() {
     const res = await Auth.signInWithPopup(Service.providerGoogle);
@@ -30,4 +30,32 @@ export default {
     return Auth.signOut();
   },
 
+  /**
+   * Realiza el logín en base a un email y un password
+   * @param {string} email
+   * @param {string} password
+   */
+  async login(email, password) {
+    const res = await Auth.signInWithEmailAndPassword(email, password);
+    return res.user;
+  },
+
+  /**
+   * Realiza el registro de un usuario en base a un email y password
+   * @param {string} email
+   * @param {string} password
+   */
+  async register(email, password) {
+    const res = await Auth.createUserWithEmailAndPassword(email, password);
+    return res.user;
+  },
+
+  /**
+   * Recupera al password dado un email
+   * https://firebase.google.com/docs/reference/js/firebase.auth.Auth#sendpasswordresetemail
+   * @param {string} email
+   */
+  async reset(email) {
+    return Auth.sendPasswordResetEmail(email);
+  },
 };
