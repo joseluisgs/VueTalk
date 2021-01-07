c461e0302dcca10002d0bc863047656e4744561f
joseluisgs <joseluisgonsan@gmail.com>
2021-01-07 15:55:38 +0100

    Eliminar console en produccion

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,12 @@
+const plugins = [];
+// remove console outputs in production enviroment!
+if (process.env.VUE_APP_STAGE === 'production') {
+  plugins.push('transform-remove-console');
+}
+
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset',
   ],
+  plugins,
 };
